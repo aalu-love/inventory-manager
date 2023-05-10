dc2c0cacb545d32eaa51786a733c5c43d9d64093
SlingShot <sanjubodra1420@gmail.com>
2023-05-10 10:23:46 +0530

    ssa

---
+++ lib/components/card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CardList extends StatelessWidget {
+  const CardList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart' show rootBundle;
 
 import 'package:csv/csv.dart';
+import 'package:flutter_application_1/components/card.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,11 +30,11 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<List<dynamic>> _data = [];
-  List<List<dynamic>> _filteredData = [];
-  List<List<dynamic>> _catagoryList = [];
+  // List<List<dynamic>> _filteredData = [];
+  // List<List<dynamic>> _catagoryList = [];
 
-  final TextEditingController _searchController = TextEditingController();
-  String _sortValue = 'Name';
+  // final TextEditingController _searchController = TextEditingController();
+  // String _sortValue = 'Name';
 
   // This function is triggered when the floating button is pressed
   void _loadCSV() async {
@@ -45,119 +46,103 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-  void _filterData(String query) {
-    List<List<dynamic>> _newList = [];
-    _data.forEach((row) {
-      if (row[6].toString().toLowerCase().contains(query.toLowerCase())) {
-        _newList.add(row);
-      }
-      if (row[2].toString().toLowerCase().contains(query.toLowerCase())) {
-        _newList.add(row);
-      }
-    });
-    setState(() {
-      _filteredData = _newList;
-    });
-  }
+  // void _filterData(String query) {
+  //   List<List<dynamic>> _newList = [];
+  //   _data.forEach((row) {
+  //     if (row[6].toString().toLowerCase().contains(query.toLowerCase())) {
+  //       _newList.add(row);
+  //     }
+  //     if (row[2].toString().toLowerCase().contains(query.toLowerCase())) {
+  //       _newList.add(row);
+  //     }
+  //   });
+  //   setState(() {
+  //     _filteredData = _newList;
+  //   });
+  // }
 
-  void _CatalogList(String catalog) {
-    List<List<dynamic>> _catalogList = [];
-    _data.forEach((row) {
-      if (row[1].toString().toLowerCase().contains(catalog.toLowerCase())) {
-        _catagoryList.add(row);
-      }
-    });
-  }
+  // void _CatalogList(String catalog) {
+  //   List<List<dynamic>> _catalogList = [];
+  //   _data.forEach((row) {
+  //     if (row[1].toString().toLowerCase().contains(catalog.toLowerCase())) {
+  //       _catagoryList.add(row);
+  //     }
+  //   });
+  // }
 
-    void _sortData(String sortValue) {
-    setState(() {
-      _sortValue = sortValue;
-      _filteredData.sort((a, b) => a[1].compareTo(b[1]));
-      if (sortValue == 'Reverse Name') {
-        _filteredData = _filteredData.reversed.toList();
-      }
-    });
-  }
+  // void _sortData(String sortValue) {
+  //   setState(() {
+  //     _sortValue = sortValue;
+  //     _filteredData.sort((a, b) => a[1].compareTo(b[1]));
+  //     if (sortValue == 'Reverse Name') {
+  //       _filteredData = _filteredData.reversed.toList();
+  //     }
+  //   });
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Kindacode.com"),
-        actions: [
-          IconButton(
-            onPressed: () {
-              _searchController.clear();
-              _filterData('');
-            },
-            icon: const Icon(Icons.clear),
-          ),
-        ],
       ),
+      // body: Column(
+      //   children: [
+      //     Padding(
+      //       padding: const EdgeInsets.all(8.0),
+      //       child: Row(
+      //         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //       ),
+      //     ),
+      //     Expanded(
+      //       child: ListView.builder(
+      //         itemBuilder: (_, index) {
+      //           return Card(
+      //             margin: const EdgeInsets.all(3),
+      //             color: index == 0 ? Colors.amber : Colors.white,
+      //             child: ListTile(
+      //               leading: Text(_data[index][0].toString()),
+      //               title: Text(_data[index][1]),
+      //               trailing: Text(_data[index][2].toString()),
+      //             ),
+      //           );
+      //         },
+      //       ),
+      //     ),
+      //   ],
+      // ),
       body: Column(
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Flexible(
-                  flex: 4,
-                  child: TextField(
-                    controller: _searchController,
-                    onChanged: (query) {
-                      _filterData(query);
-                    },
-                    decoration: InputDecoration(
-                      hintText: "Search",
-                    ),
-                  ),
-                ),
-                const Spacer(),
-                Flexible(
-                  flex: 2,
-                  child: DropdownButton<String>(
-                    value: _sortValue,
-                    icon: const Icon(Icons.arrow_downward),
-                    iconSize: 24,
-                    elevation: 16,
-                    underline: Container(
-                      height: 2,
-                      color: Theme.of(context).primaryColor,
-                    ),
-                    onChanged: (String? newValue) {
-                      _sortData(newValue!);
-                    },
-                    items: <String>[
-                      'Name',
-                      'Reverse Name',
-                    ].map<DropdownMenuItem<String>>((String value) {
-                      return DropdownMenuItem<String>(
-                        value: value,
-                        child: Text(value),
-                      );
-                    }).toList(),
-                  ),
-                ),
-              ],
             ),
           ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: _filteredData.length,
-              itemBuilder: (_, index) {
-                return Card(
-                  margin: const EdgeInsets.all(3),
-                  color: index == 0 ? Colors.amber : Colors.white,
-                  child: ListTile(
-                    leading: Text(_filteredData[index][0].toString()),
-                    title: Text(_filteredData[index][1]),
-                    trailing: Text(_filteredData[index][2].toString()),
-                  ),
-                );
-              },
-            ),
-          ),
+          Expanded(child: ListView.builder(itemBuilder: (_, index) {
+            return const SizedBox(
+              child: Text(_data[0][index], key: Int(index), ),
+            )
+            // return Card(
+            //   margin: const EdgeInsets.all(3),
+            //   color: index == 0 ? Colors.amber : Colors.white,
+            //   child: ListTile(
+            //     leading: Text(
+            //       _data[index][0].toString(),
+            //     ),
+            //     title: Text(
+            //       _data[index][1],
+            //       overflow: TextOverflow.ellipsis,
+            //       textAlign: TextAlign.left,
+            //     ),
+            //     trailing: Text(
+            //       _data[index][2].toString(),
+            //       overflow: TextOverflow.ellipsis,
+            //     ),
+            //     minLeadingWidth: 50,
+            //   ),
+            // );
+          })),
         ],
       ),
       floatingActionButton: FloatingActionButton(
